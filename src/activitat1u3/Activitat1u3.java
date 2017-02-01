68c5cc9d69d200c8612dde865c7f571e37cc2f88
ericdote <rovx15@gmail.com>
2017-02-01 18:57:28 +0100

    Fin

--- src/activitat1u3/Activitat1u3.java
+++ src/activitat1u3/Activitat1u3.java
@@ -1,16 +1,14 @@
 package activitat1u3;
 
 import java.net.InetAddress;
-
+import java.net.UnknownHostException;
 
 public class Activitat1u3 {
 
+    public static void main(String[] args) throws UnknownHostException {
 
-    public static void main(String[] args) {
-        
-        InetAddress ia;
-                
-        
+        for (InetAddress inetAddress : InetAddress.getAllByName("www.twitter.com")) {
+            System.out.println(inetAddress);
+        }
     }
-    
 }
