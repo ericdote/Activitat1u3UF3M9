31b59adc3544856ee51c792a19166f49f823e710
ericdote <rovx15@gmail.com>
2017-02-01 18:58:29 +0100

    Fin

--- src/activitat1u3/Activitat1u3.java
+++ src/activitat1u3/Activitat1u3.java
@@ -6,7 +6,7 @@ import java.net.UnknownHostException;
 public class Activitat1u3 {
 
     public static void main(String[] args) throws UnknownHostException {
-
+        //Recorremos un array de InetAddress para sacar todas las caracteristicas de las diferentes IP's que puedan tener el host.
         for (InetAddress inetAddress : InetAddress.getAllByName("www.twitter.com")) {
             System.out.println(inetAddress);
         }
